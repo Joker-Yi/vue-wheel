61254ee0cad26577a8e1602e7a77cae179095267
Joker-Yi <735734221@qq.com>
2020-03-09 01:30:11 +0800

    创建 button-group.vue 组件

--- src/app.js
+++ src/app.js
@@ -4,10 +4,12 @@
 import Vue from 'vue'
 import Button from './button'
 import Icon from './icon'
+import ButtonGroup from './button-group'
 
 // 注册一个全局的组件Button
 Vue.component('vw-button',Button)
 Vue.component('vw-icon',Icon)
+Vue.component('vw-button-group',ButtonGroup)
 
 new Vue({
   el: '#app',
---
+++ src/button-group.vue
@@ -0,0 +1,34 @@
+<template>
+	<div class="vw-button-group">
+		<slot></slot>
+	</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped lang="scss">
+	.vw-button-group {
+		display: inline-flex;
+		vertical-align: middle;
+		> .vw-button {
+			margin-left: -1px;
+			border-radius: 0;
+			&:first-child {
+				border-top-left-radius: var(--border-radius);
+				border-bottom-left-radius: var(--border-radius);
+			}
+			&:last-child {
+				border-top-right-radius: var(--border-radius);
+				border-bottom-right-radius: var(--border-radius);
+			}
+			&:hover {
+				position: relative;
+				z-index: 1;
+			}
+		}
+	}
+</style>
