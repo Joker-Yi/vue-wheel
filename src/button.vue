6c9635c02a2a7398ca324b9134965221dedfca17
Joker-Yi <735734221@qq.com>
2020-03-08 17:34:57 +0800

    为button 组件添加 icon 和 iconPosition 属性

--- src/button.vue
+++ src/button.vue
@@ -1,10 +1,25 @@
 <template>
-	<button class="vw-button">按钮</button>
+	<button class="vw-button" :class="{[`icon-${iconPosition}`]:true}">
+		<svg v-if="icon" class="icon">
+			<use :xlink:href="`#i-${icon}`"></use>
+		</svg>
+		<div class="content">
+			<slot></slot>
+		</div>
+	</button>
 </template>
 
 <script>
 export default {
-	name: ''
+	name: '',
+	// props: ['icon','iconPosition']
+	props: {
+	  icon: {},
+    iconPosition: {
+	    type: String,
+			default: 'left'
+		}
+	}
 }
 </script>
 
@@ -16,6 +31,10 @@ export default {
 		border-radius: var(--border-radius);
 		border: 1px solid var(--border-color);
 		background: var(--button-bg);
+		display: inline-flex;
+		justify-content: center;
+		align-items: center;
+		vertical-align: middle;
 		&:hover {
 			border-color: var(--border-color-hover);
 		}
@@ -25,6 +44,28 @@ export default {
 		&:focus {
 			outline: none;
 		}
-	}
+		> .icon {
+			order: 1;
+			margin-right: 0.1em;
+			margin-left: 0;
+		}
+		> .content {
+			order: 2;
+		}
 
+		&.icon-right {
+			> .icon {
+				order: 2;
+				margin-right: 0;
+				margin-left: 0.1em;
+			}
+			> .content {
+				order: 1;
+			}
+		}
+	}
+	.icon {
+		width: 1em;
+		height: 1em;
+	}
 </style>
