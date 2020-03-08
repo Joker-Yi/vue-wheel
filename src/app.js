6134677aa3e756e61b30415fb4eed16ab3b5eb07
Joker-Yi <735734221@qq.com>
2020-03-08 16:32:55 +0800

    配置parcel & 将button改为单文件组件

---
+++ src/app.js
@@ -0,0 +1,23 @@
+/**
+ * Created by Yee .
+ */
+import Vue from 'vue'
+import Button from './button'
+
+// 注册一个全局的组件Button
+Vue.component('vw-button',Button)
+
+new Vue({
+  el: '#app'
+})
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/button.vue
@@ -0,0 +1,30 @@
+<template>
+	<button class="vw-button">按钮</button>
+</template>
+
+<script>
+export default {
+	name: ''
+}
+</script>
+
+<style scoped lang="scss">
+	.vw-button {
+		font-size: var(--font-size);
+		height: var(--button-height);
+		padding: 0 1em;
+		border-radius: var(--border-radius);
+		border: 1px solid var(--border-color);
+		background: var(--button-bg);
+		&:hover {
+			border-color: var(--border-color-hover);
+		}
+		&:active {
+			background-color: var(--button-active-bg);
+		}
+		&:focus {
+			outline: none;
+		}
+	}
+
+</style>
