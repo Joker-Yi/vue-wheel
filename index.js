d88e692777e44592dff613f0cfcf57426f7be731
Yee <yiyouhua994@163.com>
2020-08-01 00:43:51 +0800

    发布npm包

---
+++ index.js
@@ -0,0 +1,23 @@
+/**
+ * Created by Yee on 2020/7/31 23:58.
+ */
+
+import Button from './src/button'
+import ButtonGroup from './src/button-group'
+import Icon from './src/icon'
+
+// node 目前不支持import 需Babel转义
+
+export {
+  Button,
+  ButtonGroup,
+  Icon
+}
+
+
+
+
+
+
+
+
