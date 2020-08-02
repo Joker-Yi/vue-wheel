de5806cc2d6a6ee20ba66790b4f4ddedf3a66c54
Joker-Yi <735734221@qq.com>
2020-08-02 11:43:13 +0800

    大体实现col/row组件

--- src/col.vue
+++ src/col.vue
@@ -2,8 +2,12 @@
  * Create by Yee on 2020/8/2 10:44
  -->
 <template>
-  <div class="col" :class="[`col-${span}`]">
-    <slot></slot>
+  <div class="col"
+       :class="[span && `col-${span}`, offset && `offset-${offset}`]"
+       :style="{paddingLeft: gutter/2+'px', paddingRight: gutter/2+'px'}">
+    <div style="border: 1px solid green; height: 100px;">
+      <slot></slot>
+    </div>
   </div>
 </template>
 
@@ -13,6 +17,14 @@
     props: {
       span: {
         type: [Number, String]
+      },
+      offset: {
+        type: [Number, String]
+      }
+    },
+    data() {
+      return {
+        gutter: 0,
       }
     }
   }
@@ -21,15 +33,20 @@
 <style scoped lang="scss">
   .col {
     height: 100px;
-    background: #139fe0;
     width: 50%;
-    border: 1px solid red;
     $class-prefix: col-;
     /*for循环 */
     @for $n from 1 through 24 {
       /*插值语法*/
       &.#{$class-prefix}#{$n} {
         width: ($n / 24) * 100%;
+      }
+    }
+    /*单个 col 的偏移*/
+    $class-prefix: offset-;
+    @for $n from 1 through 24 {
+      &.#{$class-prefix}#{$n} {
+        margin-left: ($n / 24) * 100%;
       }
     }
   }
--- src/row.vue
+++ src/row.vue
@@ -2,14 +2,28 @@
  * Create by Yee on 2020/8/2 10:43
  -->
 <template>
-  <div class="row">
+  <div class="row"
+       :style="{marginLeft: -gutter/2+'px', marginRight: -gutter/2+'px'}">
     <slot></slot>
   </div>
 </template>
 
 <script>
   export default {
-    name: 'vw-row'
+    name: 'vw-row',
+    props: {
+      gutter: {
+        type: [Number, String]
+      }
+    },
+    created () {
+    },
+    mounted () {
+      // 遍历孩子节点，赋值每个child 的 gutter
+      this.$children.forEach((vm) => {
+        vm.gutter = this.gutter
+      })
+    }
   }
 </script>
 
