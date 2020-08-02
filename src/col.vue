09cb978924a00202f0b7273a223f7537d7763732
Joker-Yi <735734221@qq.com>
2020-08-02 10:47:35 +0800

    创建row和col组件

---
+++ src/col.vue
@@ -0,0 +1,16 @@
+<!--
+ * Create by Yee on 2020/8/2 10:44
+ -->
+<template>
+  <div></div>
+</template>
+
+<script>
+  export default {
+    name: 'vw-col'
+  }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/row.vue
@@ -0,0 +1,16 @@
+<!--
+ * Create by Yee on 2020/8/2 10:43
+ -->
+<template>
+  <div></div>
+</template>
+
+<script>
+  export default {
+    name: 'vw-row'
+  }
+</script>
+
+<style scoped>
+
+</style>
