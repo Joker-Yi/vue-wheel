e7089699b0c363064903e012c757982c2dfc8f81
Joker-Yi <735734221@qq.com>
2020-08-02 22:41:18 +0800

    基本实现响应式

--- src/col.vue
+++ src/col.vue
@@ -10,6 +10,16 @@
 </template>
 
 <script>
+  let validator = (value) => {
+    let keys = Object.keys(value)
+    let valid = true
+    keys.forEach(key => {
+      if (!['span', 'offset'].includes(key)) {
+        valid = false
+      }
+    })
+    return valid
+  }
   export default {
     name: 'vw-col',
     props: {
@@ -18,7 +28,12 @@
       },
       offset: {
         type: [Number, String]
-      }
+      },
+      phone: {type: Object, validator,},
+      ipad: {type: Object, validator,},
+      narrowPc: {type: Object, validator,},
+      pc: {type: Object, validator,},
+      widePc: {type: Object, validator,}
     },
     data() {
       return {
@@ -29,15 +44,20 @@
       // 不能放在data里，因为要根据gutter的值变了 colStyle也要随之变动
       colStyle() {
         return {
-          paddingLeft: this.gutter/2+'px',
-          paddingRight: this.gutter/2+'px'
+          paddingLeft: this.gutter / 2 + 'px',
+          paddingRight: this.gutter / 2 + 'px',
         }
       },
       colClass() {
-        let {span, offset} = this
+        let {span, offset, phone, ipad, narrowPc, pc, widePc} = this
         return [
           span && `col-${span}`,
-          offset && `offset-${offset}`
+          offset && `offset-${offset}`,
+          ... (phone && [`col-phone-${phone.span}`]),
+          ... (ipad && [`col-ipad-${ipad.span}`]),
+          ... (narrowPc && [`col-narrow-pc-${narrowPc.span}`]),
+          ... (pc && [`col-pc-${pc.span}`]),
+          ... (widePc && [`col-wide-pc-${widePc.span}`]),
         ]
       }
     }
@@ -59,6 +79,81 @@
     @for $n from 1 through 24 {
       &.#{$class-prefix}#{$n} {
         margin-left: ($n / 24) * 100%;
+      }
+    }
+    /*phone*/
+    @media (max-width: 576px) {
+      $class-prefix: col-phone-;
+      @for $n from 1 through 24 {
+        &.#{$class-prefix}#{$n} {
+          width: ($n / 24) * 100%;
+        }
+      }
+      $class-prefix: offset-phone-;
+      @for $n from 1 through 24 {
+        &.#{$class-prefix}#{$n} {
+          margin-left: ($n / 24) * 100%;
+        }
+      }
+    }
+    /*pad*/
+    @media (min-width: 577px) and (max-width: 768px) {
+      $class-prefix: col-ipad-;
+      @for $n from 1 through 24 {
+        &.#{$class-prefix}#{$n} {
+          width: ($n / 24) * 100%;
+        }
+      }
+      $class-prefix: offset-ipad-;
+      @for $n from 1 through 24 {
+        &.#{$class-prefix}#{$n} {
+          margin-left: ($n / 24) * 100%;
+        }
+      }
+    }
+    /*窄屏pc*/
+    @media (min-width: 769px) and (max-width: 992px) {
+      $class-prefix: col-narrow-pc-;
+      @for $n from 1 through 24 {
+        &.#{$class-prefix}#{$n} {
+          width: ($n / 24) * 100%;
+        }
+      }
+      $class-prefix: offset-narrow-pc-;
+      @for $n from 1 through 24 {
+        &.#{$class-prefix}#{$n} {
+          margin-left: ($n / 24) * 100%;
+        }
+      }
+    }
+    /*pc*/
+    @media (min-width: 993px) and (max-width: 1200px) {
+      $class-prefix: col-pc-;
+      @for $n from 1 through 24 {
+        &.#{$class-prefix}#{$n} {
+          width: ($n / 24) * 100%;
+        }
+      }
+      $class-prefix: offset-pc-;
+      @for $n from 1 through 24 {
+        &.#{$class-prefix}#{$n} {
+          margin-left: ($n / 24) * 100%;
+        }
+      }
+    }
+    /*宽屏pc*/
+    @media (min-width: 1201px) {
+      $class-prefix: col-wide-pc-;
+      @for $n from 1 through 24 {
+        &.#{$class-prefix}#{$n} {
+          width: ($n / 24) * 100%;
+        }
+      }
+      $class-prefix: offset-wide-pc-;
+      @for $n from 1 through 24 {
+        &.#{$class-prefix}#{$n} {
+          margin-left: ($n / 24) * 100%;
+        }
       }
     }
   }
--- src/row.vue
+++ src/row.vue
@@ -45,6 +45,7 @@
 <style scoped lang="scss">
   .row{
     display: flex;
+    flex-wrap: wrap;
     &.align-left {
       justify-content: flex-start;
     }
