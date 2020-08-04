b02eefa90907014173f5cec55dd09e66c82893e9
Joker-Yi <735734221@qq.com>
2020-08-05 02:11:21 +0800

    添加row的测试用例

---
+++ test/row.test.js
@@ -0,0 +1,72 @@
+/**
+ * Created by Yee on 2020/8/5 1:47.
+ */
+const expect = chai.expect;
+import Vue from 'vue'
+import Row from '../src/row'
+import Col from '../src/col'
+
+Vue.config.productionTip = false
+Vue.config.devtools = false
+
+describe('Row', () => {
+
+  it('存在.', () => {
+    expect(Row).to.exist
+  })
+  it('接收 gutter 属性.', (done) => {
+    Vue.component('g-row', Row)
+    Vue.component('g-col', Col)
+    const div = document.createElement('div')
+    document.body.appendChild(div)
+    div.innerHTML = `
+      <g-row gutter="20">
+        <g-col span="12"></g-col>
+        <g-col span="12"></g-col>
+      </g-row>
+    `
+    const vm = new Vue({
+      el: div
+    })
+    // 此时打印 vm。$el.outerHTML 不准确 ，一些style属性是异步添加上的，所以需要延时获取
+    setTimeout(() => {
+      const row = vm.$el.querySelector('.row')
+      expect(getComputedStyle(row).marginLeft).to.eq('-10px')
+      expect(getComputedStyle(row).marginRight).to.eq('-10px')
+      const cols = vm.$el.querySelectorAll('.col')
+      expect(getComputedStyle(cols[0]).paddingRight).to.eq('10px')
+      expect(getComputedStyle(cols[1]).paddingLeft).to.eq('10px')
+      // 告诉测试 需要等此处异步代码执行完才关闭 最多等待2s
+      done()
+      vm.$el.remove()
+      vm.$destroy()
+    })
+  })
+  it('接收 align 属性', () => {
+    const div = document.createElement('div')
+    document.body.appendChild(div)
+    const Constructor = Vue.extend(Row)
+    const vm = new Constructor({
+      propsData: {
+        align: 'right'
+      }
+    }).$mount(div)
+    const element = vm.$el
+    expect(getComputedStyle(element).justifyContent).to.equal('flex-end')
+    div.remove()
+    vm.$destroy()
+  })
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
