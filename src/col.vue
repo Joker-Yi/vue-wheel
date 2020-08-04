9e4f108af9696390149a7f719d359207a482e260
Joker-Yi <735734221@qq.com>
2020-08-05 01:21:19 +0800

    采用移动优先思想完成响应式

--- src/col.vue
+++ src/col.vue
@@ -29,7 +29,7 @@
       offset: {
         type: [Number, String]
       },
-      phone: {type: Object, validator,},
+      // phone: {type: Object, validator,},
       ipad: {type: Object, validator,},
       narrowPc: {type: Object, validator,},
       pc: {type: Object, validator,},
@@ -53,11 +53,11 @@
         return [
           span && `col-${span}`,
           offset && `offset-${offset}`,
-          ... (phone && [`col-phone-${phone.span}`]),
-          ... (ipad && [`col-ipad-${ipad.span}`]),
-          ... (narrowPc && [`col-narrow-pc-${narrowPc.span}`]),
-          ... (pc && [`col-pc-${pc.span}`]),
-          ... (widePc && [`col-wide-pc-${widePc.span}`]),
+          // ... (phone && [`col-phone-${phone.span}`]),
+          ... (ipad ? [`col-ipad-${ipad.span}`] : []),
+          ... (narrowPc ? [`col-narrow-pc-${narrowPc.span}`] : []),
+          ... (pc ? [`col-pc-${pc.span}`] : []),
+          ... (widePc ? [`col-wide-pc-${widePc.span}`] : []),
         ]
       }
     }
@@ -81,23 +81,13 @@
         margin-left: ($n / 24) * 100%;
       }
     }
+
     /*phone*/
-    @media (max-width: 576px) {
-      $class-prefix: col-phone-;
-      @for $n from 1 through 24 {
-        &.#{$class-prefix}#{$n} {
-          width: ($n / 24) * 100%;
-        }
-      }
-      $class-prefix: offset-phone-;
-      @for $n from 1 through 24 {
-        &.#{$class-prefix}#{$n} {
-          margin-left: ($n / 24) * 100%;
-        }
-      }
-    }
+    /*注释掉手机端，默认使用手机样式*/
+
+
     /*pad*/
-    @media (min-width: 577px) and (max-width: 768px) {
+    @media (min-width: 577px) {
       $class-prefix: col-ipad-;
       @for $n from 1 through 24 {
         &.#{$class-prefix}#{$n} {
@@ -112,7 +102,7 @@
       }
     }
     /*窄屏pc*/
-    @media (min-width: 769px) and (max-width: 992px) {
+    @media (min-width: 769px) {
       $class-prefix: col-narrow-pc-;
       @for $n from 1 through 24 {
         &.#{$class-prefix}#{$n} {
@@ -127,7 +117,7 @@
       }
     }
     /*pc*/
-    @media (min-width: 993px) and (max-width: 1200px) {
+    @media (min-width: 993px) {
       $class-prefix: col-pc-;
       @for $n from 1 through 24 {
         &.#{$class-prefix}#{$n} {
