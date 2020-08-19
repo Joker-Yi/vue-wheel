b8fe7f0ab9f2e547bb09e8453ad83cae8b667cf8
Joker-Yi <735734221@qq.com>
2020-08-19 16:58:27 +0800

    只能同时存在一个toast

--- src/plugin.js
+++ src/plugin.js
@@ -3,22 +3,28 @@
  */
 import Toast from './toast'
 
+let currentToast
 export default {
   install (Vue, options) {
     Vue.prototype.$toast = function (message, toastOptions) {
-      let Constructor = Vue.extend(Toast)
-      let toast = new Constructor({
-        propsData: toastOptions
-      })
-      toast.$slots.default = [message]
-      toast.$mount()
-      document.body.appendChild(toast.$el)
+      if (currentToast) {
+        currentToast.close()
+      }
+      currentToast = createToast({Vue, message, propsData: toastOptions})
     }
   }
 }
 
 
-
+/* helpers  创建toast组件 */
+function createToast ({Vue, message, propsData}) {
+  let Constructor = Vue.extend(Toast)
+  let toast = new Constructor({propsData})
+  toast.$slots.default = [message]
+  toast.$mount()
+  document.body.appendChild(toast.$el)
+  return toast
+}
 
 
 
