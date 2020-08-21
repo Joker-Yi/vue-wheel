ff1687471320b03fba51e1eb28041370d0cf1e36
Joker-Yi <735734221@qq.com>
2020-08-21 21:33:07 +0800

    解决包裹popover的父元素事件不能够触发的bug，因为加了stop修饰符

--- src/popover.vue
+++ src/popover.vue
@@ -2,7 +2,7 @@
  * Create by Yee on 2020/8/21 10:45
  -->
 <template>
-  <div class="popover" @click.stop="handleClick">
+  <div class="popover" @click="handleClick" ref="popover">
     <div ref="contentWrapper" class="content-wrapper" v-if="visible">
       <slot name="content"></slot>
     </div>
@@ -21,22 +21,42 @@
       }
     },
     methods: {
-      handleClick () {
-        this.visible = !this.visible
-        if (this.visible === true) { // 如果显示content，为document添加监听click事件，可点击document隐藏content
-          this.$nextTick(() => {
-            document.body.appendChild(this.$refs.contentWrapper)
-            let {width, height, top, left} = this.$refs.triggerWrapper.getBoundingClientRect()
-            this.$refs.contentWrapper.style.left = left + window.scrollX + 'px'
-            this.$refs.contentWrapper.style.top = top + window.scrollY + 'px'
-            let eventHandler = () => {
-              this.visible = false
-              document.removeEventListener('click', eventHandler)
-            }
-            document.addEventListener('click', eventHandler)
-          })
+      handleClick (event) {
+        if (this.$refs.triggerWrapper.contains(event.target)) {
+          if (this.visible === true) {
+            this.close()
+          } else {
+            this.open()
+          }
         }
-      }
+      },
+      // 移动弹出层，并计算定位的位置
+      positionContent () {
+        document.body.appendChild(this.$refs.contentWrapper)
+        let {width, height, top, left} = this.$refs.triggerWrapper.getBoundingClientRect()
+        this.$refs.contentWrapper.style.left = left + window.scrollX + 'px'
+        this.$refs.contentWrapper.style.top = top + window.scrollY + 'px'
+      },
+      onClickDocument (e) {
+        // 当移动到content区域不隐藏
+        if (this.$refs.popover &&
+            (this.$refs.popover === e.target || this.$refs.popover.contains(e.target))
+        ) {
+          return
+        }
+        this.close()
+      },
+      open () {
+        this.visible = true
+        this.$nextTick(() => {
+          this.positionContent()
+          document.addEventListener('click', this.onClickDocument)
+        })
+      },
+      close () {
+        this.visible = false
+        document.removeEventListener('click', this.onClickDocument)
+      },
     }
   }
 </script>
