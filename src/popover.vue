559530a41f07a19cebbceb72550e6c6e022cdf85
Joker-Yi <735734221@qq.com>
2020-08-21 20:37:01 +0800

    解决当父级层overflow为hidden时popover内容被遮盖的bug

--- src/popover.vue
+++ src/popover.vue
@@ -3,10 +3,12 @@
  -->
 <template>
   <div class="popover" @click.stop="handleClick">
-    <div class="content-wrapper" v-if="visible">
+    <div ref="contentWrapper" class="content-wrapper" v-if="visible">
       <slot name="content"></slot>
     </div>
-    <slot></slot>
+    <span ref="triggerWrapper">
+      <slot></slot>
+    </span>
   </div>
 </template>
 
@@ -23,15 +25,16 @@
         this.visible = !this.visible
         if (this.visible === true) { // 如果显示content，为document添加监听click事件，可点击document隐藏content
           this.$nextTick(() => {
+            document.body.appendChild(this.$refs.contentWrapper)
+            let {width, height, top, left} = this.$refs.triggerWrapper.getBoundingClientRect()
+            this.$refs.contentWrapper.style.left = left + window.scrollX + 'px'
+            this.$refs.contentWrapper.style.top = top + window.scrollY + 'px'
             let eventHandler = () => {
               this.visible = false
-              console.log('document 隐藏 popover')
               document.removeEventListener('click', eventHandler)
             }
             document.addEventListener('click', eventHandler)
           })
-        }else{
-          console.log('vm 隐藏 popover')
         }
       }
     }
@@ -43,12 +46,11 @@
     display: inline-block;
     vertical-align: top;
     position: relative;
-    .content-wrapper {
-      position: absolute;
-      bottom: 100%;
-      left: 0;
-      border: 1px solid red;
-      box-shadow: 0 0 3px rgba(0, 0, 0, 0.5);
-    }
+  }
+  .content-wrapper {
+    position: absolute;
+    border: 1px solid red;
+    box-shadow: 0 0 3px rgba(0, 0, 0, 0.5);
+    transform: translateY(-100%);
   }
 </style>
