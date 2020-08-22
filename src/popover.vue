caf8470325fa1b52d93f1681326d83bbaac99f39
Joker-Yi <735734221@qq.com>
2020-08-23 00:20:52 +0800

    解决抖动问题

--- src/popover.vue
+++ src/popover.vue
@@ -162,10 +162,12 @@
       }
       &::before {
         border-top-color: black;
+        border-bottom: none;
         top: 100%;
       }
       &::after {
         border-top-color: white;
+        border-bottom: none;
         top: calc(100% - 1px);
       }
     }
@@ -176,10 +178,12 @@
       }
       &::before {
         border-bottom-color: black;
+        border-top: none;
         bottom: 100%;
       }
       &::after {
         border-bottom-color: white;
+        border-top: none;
         bottom: calc(100% - 1px);
       }
     }
@@ -192,10 +196,12 @@
       }
       &::before {
         border-left-color: black;
+        border-right: none;
         left: 100%;
       }
       &::after {
         border-left-color: white;
+        border-right: none;
         left: calc(100% - 1px);
       }
     }
@@ -207,10 +213,12 @@
       }
       &::before {
         border-right-color: black;
+        border-left: none;
         right: 100%;
       }
       &::after {
         border-right-color: white;
+        border-left: none;
         right: calc(100% - 1px);
       }
     }
