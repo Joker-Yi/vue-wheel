d99a1e3eba9b5659d82b6734c579f1471459340f
Joker-Yi <735734221@qq.com>
2020-08-23 11:56:03 +0800

    添加popover测试用例，遗留主动触发onmouseenter事件测试

---
+++ test/popover.test.js
@@ -0,0 +1,77 @@
+/**
+ * Created by Yee on 2020/8/23 11:49.
+ */
+
+
+const expect = chai.expect;
+import Vue from 'vue'
+import Popover from '../src/popover'
+
+Vue.config.productionTip = false
+Vue.config.devtools = false
+
+describe('Popover', () => {
+
+  it('存在.', () => {
+    expect(Popover).to.exist
+  })
+
+  it('可以设置position.', (done) => {
+    Vue.component('vw-popover', Popover)
+    const div = document.createElement('div')
+    document.body.appendChild(div)
+    div.innerHTML = `
+    <vw-popover position="bottom" ref="a">
+      <template slot="content">
+      弹出内容
+      </template>
+      <button>点我</button>
+    </vw-popover>
+    `
+    const vm = new Vue({
+      el: div
+    })
+    vm.$el.querySelector('button').click()
+    vm.$nextTick(() => {
+      const {contentWrapper} = vm.$refs.a.$refs
+      expect(contentWrapper.classList.contains('position-bottom')).to.be.true
+      done()
+    })
+  })
+  xit('可以设置 trigger', (done) => {
+    Vue.component('vw-popover', Popover)
+    const div = document.createElement('div')
+    document.body.appendChild(div)
+    div.innerHTML = `
+    <vw-popover trigger="hover" ref="a">
+      <template slot="content">
+      弹出内容
+      </template>
+      <button>点我</button>
+    </vw-popover>
+    `
+    const vm = new Vue({
+      el: div
+    })
+    setTimeout(() => {
+      let event = new Event('mouseenter');
+      vm.$el.dispatchEvent(event)
+      vm.$nextTick(() => {
+        const {contentWrapper} = vm.$refs.a.$refs
+        expect(contentWrapper).to.exist
+        done()
+      })
+    }, 200)
+
+  })
+})
+
+
+
+
+
+
+
+
+
+
