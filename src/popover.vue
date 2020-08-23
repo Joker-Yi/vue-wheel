7801f8be4875ce9412d95e9ca9e9a89f31bc4eb2
Joker-Yi <735734221@qq.com>
2020-08-23 11:43:22 +0800

    支持内容内添加close API

--- src/popover.vue
+++ src/popover.vue
@@ -4,7 +4,7 @@
 <template>
   <div class="popover" ref="popover">
     <div ref="contentWrapper" class="content-wrapper" v-if="visible" :class="{[`position-${position}`]:true}">
-      <slot name="content"></slot>
+      <slot name="content" :close="close"></slot>
     </div>
     <span ref="triggerWrapper" style="display: inline-block;">
       <slot></slot>
